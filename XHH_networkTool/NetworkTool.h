771d7b6341cabc6d6831d049861f33b9a8f4b7dd
ithuihui <chhhithuihui@gmail.com>
2016-11-26 14:49:43 +0800

    1.创建NetworkTool

---
+++ XHH_networkTool/NetworkTool.h
@@ -0,0 +1,13 @@
+//
+//  NetworkTool.h
+//  XHH_networkTool
+//
+//  Created by xiaohuihui on 2016/11/26.
+//  Copyright © 2016年 30-陈辉华. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkTool : NSObject
+
+@end
---
+++ XHH_networkTool/NetworkTool.m
@@ -0,0 +1,13 @@
+//
+//  NetworkTool.m
+//  XHH_networkTool
+//
+//  Created by xiaohuihui on 2016/11/26.
+//  Copyright © 2016年 30-陈辉华. All rights reserved.
+//
+
+#import "NetworkTool.h"
+
+@implementation NetworkTool
+
+@end
